4935601ff34bb991ab02dac404f628bc5f030f4d
Alex Gusev <alex.desmod@gmail.com>
2025-08-26 21:16:07 +0300

    initial commit

---
+++ spostats/stats/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    spotify_id = models.CharField(max_length=64, blank=True)
+    display_name = models.CharField(max_length=255, blank=True)
+    avatar_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.display_name or self.user.username
+
+class Play(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    spotify_track_id = models.CharField(max_length=64, db_index=True)
+    track_name = models.CharField(max_length=255)
+    artist_name = models.CharField(max_length=255)
+    played_at = models.DateTimeField(db_index=True)
+    duration_ms = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        unique_together = [("user", "spotify_track_id", "played_at")]
+        ordering = ["-played_at"]
