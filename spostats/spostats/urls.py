180abdc02d08fafd57ea09565230b5ddb9d2ac67
Alex Gusev <alex.desmod@gmail.com>
2026-07-25 10:12:25 +0300

    404/500 updated, admin endpoint changed

--- spostats/spostats/urls.py
+++ spostats/spostats/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('darkside/', admin.site.urls),
     path("accounts/", include("allauth.urls")),  # allauth routes
     path("", include("stats.urls")),
 ]
